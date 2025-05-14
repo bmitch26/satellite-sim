6dd712579acf020e95b3a272a4ecb8001c856147
Brett <brtm23@gmail.com>
2025-05-14 18:49:30 -0500

    Clean up code

--- main.py
+++ main.py
@@ -9,38 +9,7 @@ from cubesat.sunlight import SunlightModelTask
 from cubesat.mode_manager import ModeManagerTask
 
 import json
-import threading
-import queue
-import os
 from rich import print
-
-# === CLI Command Input Setup ===
-# command_queue = queue.Queue()
-# parser = CommandParser()
-
-# def command_input_loop():
-#     # print("[bold purple]Ground Station CLI: Type JSON commands (or 'exit'):[/bold purple]")
-#     while True:
-#         cmd_str = input(">>> ").strip()
-#         if not cmd_str:
-#             continue  # Ignore empty input (like pressing Enter)
-
-#         if cmd_str.lower() == "exit":
-#             break
-#         elif cmd_str.lower() == "shutdown":
-#             print("[CLI] Shutdown requested. Exiting simulation...")
-#             os._exit(0)
-        
-#         try:
-#             cmd = parser.parse(cmd_str)
-#             command_queue.put(cmd)
-#         except ValueError as e:
-#             print(f"[CLI Error] {e}")
-
-
-# # Start CLI in a background thread
-# input_thread = threading.Thread(target=command_input_loop, daemon=True)
-# input_thread.start()
 
 # === Initialize shared telemetry buffer ===
 telemetry_buffer = []
